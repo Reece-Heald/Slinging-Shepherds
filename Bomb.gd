eef86e803b7739bb9e4d2cff60e1ae4d02562912
Reece-Heald <56052565+Reece-Heald@users.noreply.github.com>
2022-03-04 17:26:56 -0500

    bomb move

--- Bomb.gd
+++ Bomb.gd
@@ -9,17 +9,50 @@ var isStunBomb = true
 
 var singleton = preload("res://GameVarables.gd")
 
-export var velocity = 15
+
 export var _scaleRatio = Vector2(1,1)
 
 var damage = 5
 var bombRange = 15
+var _speed := 5
+var velocity = Vector2(0,0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("test")
 	
+	
 	 # Replace with function body.
 
+func _physics_process(delta):
+	#velocity.x =+ _speed
+	#move_and_collide(velocity)
+	#print(global_position.x,global_position.y)
+	var angle = get_angle_to(Vector2(300,300))
+	velocity.x = cos(angle)
+	velocity.y = sin(angle)
+	global_position += velocity * _speed  
+	
+func bombThrow(startPos, targetPos):
+	
+	#first pos
+	var startPosX = startPos.x 
+	var startPosY = startPos.y
+	#second pos
+	var targetPosX = targetPos.x
+	var targetPosY = targetPos.y
+	
+	#|ab| = sqrt(sqr(b.x - a.x) + sqr(b.y - a.y))
+	var middleOfLineX = ceil(abs(sqrt(pow(targetPosX - startPosX,2))))
+	var middleOfLineY  = ceil(abs(sqrt(pow(targetPosY - startPosY,2))))
+	var midPoint = Vector2(middleOfLineX, middleOfLineY)
+	
+	
+	
+	#movement part
+	#self.global_position = startPos
+	var direction = (targetPos - startPos).normalized()
+	
 
 
 func _on_Area2D_body_entered(body):
--- test_bomb.gd
+++ test_bomb.gd
@@ -6,15 +6,19 @@ extends Node2D
 # var b = "text"
 var bomb = load("res://Bomb.tscn")
 var targetPost = Vector2(global_position.x+100, global_position.y)
+var newBomb = bomb.instance()
 
 var _scaleRatio = Vector2(1,1)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var newBomb = bomb.instance()
 	get_parent().add_child(newBomb)
+	
+	
+func _physics_process(delta):
+
+	pass
 	#newBomb.bombThrow(self.global_position,targetPost, scale)
-
-
+	#newBomb.bombThrow(newBomb.global_position, targetPost)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
