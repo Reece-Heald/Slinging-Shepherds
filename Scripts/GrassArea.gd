c6dd7d53527b5591798b2186526ac62ed8bc6854
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-03-22 19:48:36 -0400

    grass area works, still using counters

--- Scripts/GrassArea.gd
+++ Scripts/GrassArea.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 export (int) var grassSpawnSpeed = 60
 
@@ -7,15 +7,11 @@ var screenSizeVert
 const grassLocation = preload("res://Prefabs/Grass.tscn")
 var grassSprite
 var counter
-var position
 var areaSize
 
-func _init():
-	#areaSize = get_parent().areaSize
-	#position = self.translation
-	pass
 
 func _ready():
+	areaSize = get_parent().areaSize
 	counter = 0
 	
 	screenSizeHoriz = get_viewport().get_visible_rect().size.x
@@ -23,10 +19,10 @@ func _ready():
 	
 func _process(delta):
 	if (counter > 100):
-		#var posHoriz = self.x + (randf() - .5) * areaSize
-		#var posVert = self.y + (randf() - .5) * areaSize
-		var posHoriz = screenSizeHoriz * randf()
-		var posVert = screenSizeVert * randf()
+		var posHoriz = position.x + (randf() - .5) * areaSize
+		var posVert = position.y + (randf() - .5) * areaSize
+		#var posHoriz = screenSizeHoriz * randf()
+		#var posVert = screenSizeVert * randf()
 		grassSprite = grassLocation.instance()
 		grassSprite.set_global_position(Vector2(posHoriz, posVert))
 		add_child(grassSprite)
