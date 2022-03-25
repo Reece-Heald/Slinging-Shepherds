b7f15a92b704f263935bb753174b5e68e5b961e8
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-03-24 20:59:07 -0400

    grass spawning set up for game

--- Scripts/Grass.gd
+++ Scripts/Grass.gd
@@ -8,7 +8,7 @@ var grow3 = preload("res://Sprites/Grass/Grass3.png")
 
 export (int) var timeToSprite2 = 3
 export (int) var timeToSprite3 = 6
-export (int) var timeToDeath = 10
+#export (int) var timeToDeath = 10
 
 var stage = 1
 
@@ -24,9 +24,18 @@ func _on_Timer_timeout():
 		set_texture(grow2)
 	if (stage == timeToSprite3):
 		set_texture(grow3)
-	if (stage == timeToDeath):
 		$Timer.stop()
-		get_parent().GrassDies()
-#		print("grass dies: ", position)
-		queue_free()
+#	if (stage == timeToDeath):
+#		$Timer.stop()
+#		get_parent().GrassDies()
+##		print("grass dies: ", position)
+#		queue_free()
 	stage += 1
+	
+
+# if you click and the mouse is near grass, the grass dies
+func _input(event):
+	if event is InputEventMouseButton:
+		if (abs(event.position.x - global_position.x) < 10 && abs(event.position.y - global_position.y) < 10):
+			get_parent().GrassDies()
+			queue_free()
