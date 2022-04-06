fa520d346a9496c538217e13902948949922da7a
igniseros <42523737+igniseros@users.noreply.github.com>
2022-04-06 18:43:00 -0400

    Update Sheep.gd

--- Scripts/Sheep.gd
+++ Scripts/Sheep.gd
@@ -1,10 +1,11 @@
 extends MovingCharacter
 
-export var perterbation_scale : float = PI/3
+export var random_perterbation_scale : float = PI/3
 export var look_back_number : int = 8
 export var herding_radius : float = 90
 export var debug_draw : bool = false
 export var look_at_player_num : int = 1
+export var flip_threshold : float = .1
 var connected = false
 var direction = 0
 
@@ -19,11 +20,15 @@ func _ready():
 
 func _process(delta):
 	._process(delta)
+	#if we are not going towards our taget position (meaning we are colliding with something)
+	if (direction_real - (target_pos-global_position).normalized()).length() > flip_threshold:
+		direction = direction - PI
+	
+	target_pos = global_position + Vector2(0,200).rotated(direction)
 	update()
 
 func _on_Turn_Timer_timeout():
-	direction += (randf() - .5) * perterbation_scale
-	target_pos = global_position + Vector2(0,200).rotated(direction)
+	direction += (randf() - .5) * random_perterbation_scale
 
 func _player_position_updated(who, positions: Array):
 	var combined_direction = Vector2(0,0)
@@ -34,9 +39,10 @@ func _player_position_updated(who, positions: Array):
 	
 	if combined_direction != Vector2(0,0):
 		direction = combined_direction.angle() + PI/2
-		target_pos = global_position + Vector2(0,200).rotated(direction)
 
 func _draw():
 	if debug_draw:
 		draw_circle(Vector2(0,0), herding_radius, Color(1,1,0,.1))
 		draw_line(Vector2(0,0), target_pos - position, Color(1,0,0))
+		draw_line(Vector2(0,0), direction_real * 100, Color(1,1,0))
+		draw_line(Vector2(0,0), Vector2(0,200).rotated(direction), Color(0,1,1))
