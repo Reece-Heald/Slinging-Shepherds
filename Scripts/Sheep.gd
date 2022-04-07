07491fdeb945eb96f5092089a9a5eeeadc6bb9df
igniseros <42523737+igniseros@users.noreply.github.com>
2022-04-07 12:06:20 -0400

    sheep go towards grass

--- Scripts/Sheep.gd
+++ Scripts/Sheep.gd
@@ -6,8 +6,12 @@ export var herding_radius : float = 90
 export var debug_draw : bool = false
 export var look_at_player_num : int = 1
 export var flip_threshold : float = .1
+export var grass_attraction_power : float = .85
 var connected = false
+var just_herded : bool = false
 var direction = 0
+var known_grasses = []
+var best_grass = null
 
 func _ready():
 	#called when a new grass enters the chat
@@ -26,11 +30,43 @@ func _process(delta):
 	if (direction_real - (target_pos-global_position).normalized()).length() > flip_threshold:
 		direction = direction - PI
 	
-	target_pos = global_position + Vector2(0,200).rotated(direction)
+	target_pos = global_position + Vector2(200,0).rotated(direction)
 	update()
 
 func _on_Turn_Timer_timeout():
 	direction += (randf() - .5) * random_perterbation_scale
+	
+	if state == MOVING:
+		attract_to_grass()
+
+func attract_to_grass():
+	calculate_best_grass()
+	#if we have a best grass then change the direction by the attraction power
+	if best_grass != null and not just_herded:
+		direction *= 1-grass_attraction_power
+		direction += grass_attraction_power*(global_position.angle_to_point(best_grass.global_position) + PI)
+
+
+#just finds the closest known grass
+func calculate_best_grass():
+	if known_grasses.size() == 0: 
+		best_grass = null
+		return
+	var any_real_winners : bool = false
+	best_grass = known_grasses[0]
+	var best_grass_dist = best_grass.global_position.distance_to(global_position)
+	for grass in known_grasses:
+		grass = grass as Sprite
+		#makes sure the grass is still alive
+		if grass.is_inside_tree():
+			#calc grass distance
+			var dist = grass.global_position.distance_to(global_position)
+			if dist <= best_grass_dist and dist <= herding_radius:
+				any_real_winners = true
+				best_grass = grass 
+				best_grass_dist = dist
+	if not any_real_winners: best_grass = null
+
 
 func _player_position_updated(who, positions: Array):
 	var combined_direction = Vector2(0,0)
@@ -40,13 +76,20 @@ func _player_position_updated(who, positions: Array):
 			combined_direction += global_position.direction_to(positions[-1*i]) / dist #our direction to it to the sum
 	
 	if combined_direction != Vector2(0,0):
-		direction = combined_direction.angle() + PI/2
+		just_herded = true
+		direction = combined_direction.angle() + PI
+	else:
+		just_herded = false
 
 func _draw():
 	if debug_draw:
-		draw_circle(Vector2(0,0), herding_radius, Color(1,1,0,.1))
+		var c = Color(1,1,0,.1) if not just_herded else Color(1,.25,0,.1)
+		draw_circle(Vector2(0,0), herding_radius, c)
 		draw_line(Vector2(0,0), target_pos - position, Color(1,0,0))
 		draw_line(Vector2(0,0), direction_real * 100, Color(1,1,0))
 		
+		if best_grass != null: 
+			draw_line(Vector2(0,0), best_grass.global_position - global_position, Color(1,1,1,1))
+		
 func _a_grass_has_entered_chat(grass):
-	pass
+	known_grasses.append(grass)
