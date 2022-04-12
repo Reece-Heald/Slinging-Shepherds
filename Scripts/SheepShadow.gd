1958727d3106917c234ea7edc537e759188ac1ce
Hamilton Gilbert <48567605+HamiltonGilbert@users.noreply.github.com>
2022-04-12 17:42:16 -0400

    improved sheep shadows

--- Scripts/SheepShadow.gd
+++ Scripts/SheepShadow.gd
@@ -7,7 +7,9 @@ onready var animatedSprite = get_node(path_to_animated_sprite) as AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	if (animatedSprite.animation == "walk_down" || animatedSprite.animation == "walk_up"):
+	if (animatedSprite.animation == "walk_down" || animatedSprite.animation == "walk_up" || animatedSprite.animation == "eat_up" || animatedSprite.animation == "eat_down"):
 		rotation = 90
+		position = Vector2(0,0)
 	else:
 		rotation = 0
+		position = Vector2(-1,9)
